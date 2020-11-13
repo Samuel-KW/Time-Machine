2788ff68e9e0ccec76ed818d94fff80d8fef8912
Samuel-UC <68044712+Samuel-UC@users.noreply.github.com>
2020-11-12 21:44:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
+
+
 (() => {
     
-    let old = Date.now,
-        hook = function () {
-            return old() * 10;
+    const mult = 10;
+
+    let old_date = Date.now,
+        hook_date = function () {
+            return old_date.apply(this, arguments) * mult;
         };
 
-    Date.now = hook;
+    Date.now = hook_date;
+
+
+    let old_interval = setInterval,
+        hook_interval = function () {
+
+            let args = [...arguments];
+
+            if (args[1]) args[1] /= mult
+
+            return old_interval.apply(this, args);
+        };
+
+    setInterval = hook_interval;
+
+
+    let old_timeout = setTimeout,
+        hook_timeout = function () {
+
+            let args = [...arguments];
+
+            if (args[1]) args[1] /= mult
+
+            return old_timeout.apply(this, args);
+        };
+
+    setTimeout = hook_timeout;
 
 })();
